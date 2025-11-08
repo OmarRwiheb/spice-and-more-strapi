9defed790f9dfddbb54f5b1987a50ca7352fbed3
Omar Rwiheb <omarrwiheb99@gmail.com>
2025-11-08 20:29:20 +0200

    adding description

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -445,6 +445,7 @@ export interface ApiProductProduct extends Struct.CollectionTypeSchema {
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    description: Schema.Attribute.Text;
     image: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
